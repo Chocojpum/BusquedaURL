710bf31625df92ff1f493e9e09b0d6faab75767d
José Pedro Unda Montecinos <jose.unda1801@alumnos.ubiobio.cl>
2021-12-08 17:12:11 -0300

    Creación .java

---
+++ src/BusquedaURL.java
@@ -0,0 +1,10 @@
+/*
+Autores:    Catherine Neira Saavedra
+            Fernando Sánchez Espinoza
+            José Pedro Unda Montecinos
+*/
+public class BusquedaURL{
+    public static void main(String[] args) {
+        
+    }
+}
